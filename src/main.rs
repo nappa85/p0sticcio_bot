1c05fb810896dfd8eb712e0626efc1d667a74311
Marco Napetti <nappa85@therocktrading.com>
2021-11-22 16:52:24 +0100

    LRU cache

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use chrono::Utc;
 
 use ingress_intel_rs::{Intel, plexts::{Markup, Tab}};
 
+use lru_time_cache::LruCache;
+
 use once_cell::sync::Lazy;
 
 use serde_json::json;
@@ -20,7 +22,7 @@ static BOT_TOKEN: Lazy<String> = Lazy::new(|| env::var("BOT_TOKEN").expect("Miss
 fn parse_markup(markup: &Markup) -> String {
     match markup.0.as_str() {
         "PORTAL" => {
-            let coords = format!("{},{}", markup.1.lat_e6.unwrap_or_default() as f64 / 1000.0, markup.1.lng_e6.unwrap_or_default() as f64 / 1000.0);
+            let coords = format!("{},{}", markup.1.lat_e6.unwrap_or_default() as f64 / 1000000.0, markup.1.lng_e6.unwrap_or_default() as f64 / 1000000.0);
             format!(
                 "<a href=\"https://intel.ingress.com/intel?pll={}\">{}</a> (<a href=\"https://maps.google.it/maps/?q={}\">{}</a>)",
                 coords,
@@ -36,12 +38,25 @@ fn parse_markup(markup: &Markup) -> String {
                 _ => markup.1.plain.clone(),
             }
         },
+        "TEXT" => {
+            match markup.1.plain.as_str() {
+                " captured " => format!(" {}captured ", unsafe { String::from_utf8_unchecked(vec![0xE2, 0x9B, 0xB3]) }),//flag
+                " created a Control Field @" => format!(" {}created a Control Field @", unsafe { String::from_utf8_unchecked(vec![0xE2, 0x96, 0xB3]) }),//triangle
+                " deployed a Resonator on " => format!(" {}deployed a Resonator on ", unsafe { String::from_utf8_unchecked(vec![0xF0, 0x9F, 0xA7, 0xB1]) }),//bricks
+                " destroyed a Resonator on " => format!(" {}destroyed a Resonator on ", unsafe { String::from_utf8_unchecked(vec![0xF0, 0x9F, 0x92, 0xA5]) }),//explosion
+                " destroyed the Link " => format!(" {} destroyed the Link", unsafe { String::from_utf8_unchecked(vec![0xE2, 0x9C, 0x82]) }),//scissors
+                " linked " => format!(" {}linked ", unsafe { String::from_utf8_unchecked(vec![0xF0, 0x9F, 0x94, 0x97]) }),//chain
+                "Drone returned to Agent by " => format!("{}Drone returned to Agent by ", unsafe { String::from_utf8_unchecked(vec![0xF0, 0x9F, 0x9B, 0xB8]) }),//ufo
+                _ => markup.1.plain.clone(),
+            }
+        },
         _ => markup.1.plain.clone(),
     }
 }
 
 #[tokio::main]
 async fn main() {
+    println!("Started version {}", env!("CARGO_PKG_VERSION"));
     tracing_subscriber::fmt::init();
     let url = format!("https://api.telegram.org/bot{}/sendMessage", BOT_TOKEN.as_str());
 
@@ -58,6 +73,7 @@ async fn main() {
     let client = reqwest::Client::new();
     let mut interval = time::interval(Duration::from_secs(60));
     let mut last_timestamp = Utc::now().timestamp_millis();
+    let mut sent_cache: LruCache<String, ()> = LruCache::with_expiry_duration(Duration::from_secs(120));//2 minutes cache
     loop {
         interval.tick().await;
         if let Ok(res) = intel.get_plexts([45362997, 12066414], [45747158, 12939141], Tab::All, Some(last_timestamp), None).await {
@@ -68,17 +84,23 @@ async fn main() {
 
             for (_id, time, plext) in res.result.iter().rev() {
                 if *time > last_timestamp {
-                    client.post(&url)
-                        .header("Content-Type", "application/json")
-                        .json(&json!({
-                            "chat_id": -532100731,
-                            "text": plext.plext.markup.iter().map(parse_markup).collect::<Vec<_>>().join(""),
-                            "parse_mode": "HTML"
-                        }))
-                        .send()
-                        .await
-                        .map_err(|e| error!("{}", e))
-                        .ok();
+                    let msg = plext.plext.markup.iter().map(parse_markup).collect::<Vec<_>>().join("");
+                    if sent_cache.notify_insert(msg.clone(), ()).0.is_none() {
+                        client.post(&url)
+                            .header("Content-Type", "application/json")
+                            .json(&json!({
+                                "chat_id": -532100731,
+                                "text": msg,
+                                "parse_mode": "HTML"
+                            }))
+                            .send()
+                            .await
+                            .map_err(|e| error!("{}", e))
+                            .ok();
+                    }
+                    else {
+                        debug!("Blocked duplicate entry {:?}", plext);
+                    }
                 }
                 else {
                     debug!("plext time {} and last_timestamp {}", time, last_timestamp);
