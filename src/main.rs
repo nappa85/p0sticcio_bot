2fb13c9a674db46544a9ae8b9ddd37496443448f
Marco Napetti <nappa85@therocktrading.com>
2021-11-26 22:43:30 +0100

    rate limited

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::{env, time::Duration};
+use std::{collections::HashMap, env, future, time::Duration};
 
 use chrono::Utc;
+
+use futures_util::{Stream, StreamExt, stream::unfold};
 
 use ingress_intel_rs::{Intel, plexts::Tab};
 
@@ -10,7 +12,9 @@ use once_cell::sync::Lazy;
 
 use serde_json::json;
 
-use tokio::time;
+use stream_throttle::{ThrottlePool, ThrottleRate, ThrottledStream};
+
+use tokio::{time, sync::mpsc};
 
 use tracing::{debug, error, info};
 
@@ -22,12 +26,61 @@ static PASSWORD: Lazy<Option<String>> = Lazy::new(|| env::var("PASSWORD").ok());
 static COOKIES: Lazy<Option<String>> = Lazy::new(|| env::var("COOKIES").ok());
 static BOT_TOKEN: Lazy<String> = Lazy::new(|| env::var("BOT_TOKEN").expect("Missing env var BOT_TOKEN"));
 
+fn make_stream<T>(rx: mpsc::UnboundedReceiver<T>) -> impl Stream<Item=T> {
+    unfold(rx, |mut rx| async {
+        let next = rx.recv().await?;
+        Some((next, rx))
+    })
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     println!("Started version {}", env!("CARGO_PKG_VERSION"));
     tracing_subscriber::fmt::init();
     let url = format!("https://api.telegram.org/bot{}/sendMessage", BOT_TOKEN.as_str());
     let config = config::get().await.unwrap();
+
+    let (global_tx, global_rx) = mpsc::unbounded_channel();
+    tokio::spawn(async move {
+        // we can send globally only 30 telegram messages per second
+        let rate = ThrottleRate::new(30, Duration::from_secs(1));
+        let pool = ThrottlePool::new(rate);
+        let client = reqwest::Client::new();
+        let c = &client;
+        let u = &url;
+        make_stream(global_rx).throttle(pool).for_each_concurrent(None, |(user_id, msg): (u64, String)| async move {
+            c.post(u)
+                .header("Content-Type", "application/json")
+                .json(&json!({
+                    "chat_id": user_id,
+                    "text": msg,
+                    "parse_mode": "HTML",
+                    "disable_web_page_preview": true
+                }))
+                .send()
+                .await
+                .map_err(|e| error!("{}", e))
+                .ok();
+        }).await;
+    });
+
+    let senders = config.zones.iter().map(|z| z.users.iter().map(|(id, _)| {
+        let (tx, rx) = mpsc::unbounded_channel();
+        let global_tx = global_tx.clone();
+        let id = *id;
+        tokio::spawn(async move {
+            // We can send a single message per telegram chat per second
+            let rate = ThrottleRate::new(1, Duration::from_secs(1));
+            let pool = ThrottlePool::new(rate);
+            make_stream(rx).throttle(pool).for_each(|msg| {
+                global_tx.send((id, msg))
+                    .map_err(|e| error!("Sender error: {}", e))
+                    .ok();
+                future::ready(())
+            }).await;
+        });
+        (id, tx)
+    })).flatten().collect::<HashMap<_, _>>();
 
     let mut intel = Intel::build(USERNAME.as_deref(), PASSWORD.as_deref());
 
@@ -39,7 +92,6 @@ async fn main() {
         }
     }
 
-    let client = reqwest::Client::new();
     let mut interval = time::interval(Duration::from_secs(30));
     let mut last_timestamp = vec![Utc::now().timestamp_millis(); config.zones.len()];
     let mut sent_cache: LruCache<String, ()> = LruCache::with_expiry_duration(Duration::from_secs(120));//2 minutes cache
@@ -66,17 +118,8 @@ async fn main() {
                     if sent_cache.notify_insert(msg.to_string(), ()).0.is_none() {
                         for (id, filter) in &zone.users {
                             if filter.apply(msg) {
-                                client.post(&url)
-                                    .header("Content-Type", "application/json")
-                                    .json(&json!({
-                                        "chat_id": id,
-                                        "text": msg.to_string(),
-                                        "parse_mode": "HTML",
-                                        "disable_web_page_preview": true
-                                    }))
-                                    .send()
-                                    .await
-                                    .map_err(|e| error!("{}", e))
+                                senders[id].send(msg.to_string())
+                                    .map_err(|e| error!("Sender error: {}", e))
                                     .ok();
                             }
                         }
